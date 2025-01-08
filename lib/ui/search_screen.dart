b5b1f9ad227a1a1e46e9c2883553e9d3f7c4a433
MohammadNikmard <mnikmard2004@gmail.com>
2025-01-08 15:33:45 +0330

    feature: add search screen

---
+++ lib/ui/search_screen.dart
@@ -0,0 +1,187 @@
+import 'package:book_nexsus/constants/constants.dart';
+import 'package:book_nexsus/widgets/custom_search_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  late TextEditingController searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom: 24),
+                child: CustomSearchField(
+                  controller: searchController,
+                  prefixIcon: SizedBox(
+                    height: 24,
+                    width: 24,
+                    child: Center(
+                      child: SvgPicture.asset(
+                        'assets/images/icon_arrow_left.svg',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Clear recent searches',
+                    style: TextStyle(
+                      fontFamily: 'NB',
+                      fontSize: 16,
+                      color: AppColors.white,
+                    ),
+                  ),
+                  Text(
+                    'Recent searches',
+                    style: TextStyle(
+                      fontFamily: 'NB',
+                      fontSize: 12,
+                      color: AppColors.greyMed,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                height: MediaQuery.of(context).size.height - 150,
+                child: CustomScrollView(
+                  slivers: [
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, indx) {
+                          return const _RecentSearchProduct(
+                            isAlreadySearched: true,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RecentSearchProduct extends StatelessWidget {
+  const _RecentSearchProduct({
+    required this.isAlreadySearched,
+  });
+  final bool isAlreadySearched;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 77,
+            width: 53,
+            child: Center(
+              child: Image.asset(
+                'assets/images/The Good Guy Cover.png',
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'The Good Guy',
+                style: TextStyle(
+                  fontFamily: 'NM',
+                  fontSize: 14,
+                  color: AppColors.white,
+                ),
+              ),
+              const SizedBox(height: 3),
+              const Text(
+                'The Good Guy',
+                style: TextStyle(
+                  fontFamily: 'NR',
+                  fontSize: 12,
+                  color: AppColors.white,
+                ),
+              ),
+              const SizedBox(height: 3),
+              Row(
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/icon_headphone.svg',
+                  ),
+                  const SizedBox(height: 2),
+                  const Text(
+                    '5m',
+                    style: TextStyle(
+                      fontFamily: 'NR',
+                      fontSize: 10,
+                      color: AppColors.greyLight,
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  SvgPicture.asset(
+                    'assets/images/icon_glasses.svg',
+                  ),
+                  const SizedBox(height: 2),
+                  const Text(
+                    '8m',
+                    style: TextStyle(
+                      fontFamily: 'NR',
+                      fontSize: 10,
+                      color: AppColors.greyLight,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Spacer(),
+          Visibility(
+            visible: isAlreadySearched,
+            child: GestureDetector(
+              onTap: () {},
+              child: SvgPicture.asset(
+                'assets/images/icon_delete.svg',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/custom_search_field.dart
@@ -0,0 +1,52 @@
+import 'package:book_nexsus/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomSearchField extends StatelessWidget {
+  const CustomSearchField({
+    super.key,
+    required this.controller,
+    this.prefixIcon,
+    this.hintText,
+  });
+  final TextEditingController controller;
+  final Widget? prefixIcon;
+  final String? hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 52,
+      width: double.infinity,
+      child: TextField(
+        style: const TextStyle(
+          fontFamily: 'NR',
+          fontSize: 14,
+          color: AppColors.white,
+        ),
+        decoration: InputDecoration(
+          prefixIcon: prefixIcon,
+          filled: true,
+          fillColor: AppColors.greyDark,
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            fontFamily: 'NR',
+            fontSize: 14,
+            color: AppColors.greyMed,
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1,
+              color: AppColors.greyMed,
+            ),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              width: 1,
+              color: AppColors.greenAccent,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
