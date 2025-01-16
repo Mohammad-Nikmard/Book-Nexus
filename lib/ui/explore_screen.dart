5717b00fbd90288a3b77d06e11708f35a65ab921
MohammadNikmard <mnikmard2004@gmail.com>
2025-01-16 20:54:56 +0330

    feature: add explore screen

--- lib/ui/explore_screen.dart
+++ lib/ui/explore_screen.dart
@@ -14,6 +14,20 @@ class ExploreScreen extends StatefulWidget {
 class _ExploreScreenState extends State<ExploreScreen> {
   late TextEditingController searchController;
 
+  final List<String> topicNames = [
+    'Personal growth',
+    'Culture & Society',
+    'Fiction',
+    'Mind & Philosophy',
+    'Health & Fitness',
+    'Biographies',
+    'Education',
+    'History',
+    'Future',
+    'Technology',
+    'Life style',
+  ];
+
   @override
   void initState() {
     searchController = TextEditingController();
@@ -31,23 +45,23 @@ class _ExploreScreenState extends State<ExploreScreen> {
     return Scaffold(
       backgroundColor: AppColors.background,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(bottom: 25, top: 8),
-                child: CustomDividerHeader(
-                  header: 'Explore',
-                  boxWidth: 94,
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 16, bottom: 25, top: 8),
+              child: CustomDividerHeader(
+                header: 'Explore',
+                boxWidth: 94,
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height - 75,
-                child: CustomScrollView(
-                  slivers: [
-                    SliverToBoxAdapter(
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height - 75,
+              child: CustomScrollView(
+                slivers: [
+                  SliverPadding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    sliver: SliverToBoxAdapter(
                       child: CustomSearchField(
                         height: 45,
                         controller: searchController,
@@ -63,45 +77,35 @@ class _ExploreScreenState extends State<ExploreScreen> {
                         ),
                       ),
                     ),
-                    const SliverToBoxAdapter(
-                      child: _ExploreTopicsSection(),
+                  ),
+                  SliverToBoxAdapter(
+                    child: _ExploreTopicsSection(
+                      topicNames: topicNames,
                     ),
-                  ],
-                ),
+                  ),
+                  _ExploreTopicLists(
+                    topicNames: topicNames,
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class _ExploreTopicsSection extends StatefulWidget {
-  const _ExploreTopicsSection();
+class _ExploreTopicsSection extends StatelessWidget {
+  const _ExploreTopicsSection({
+    required this.topicNames,
+  });
+  final List<String> topicNames;
 
-  @override
-  State<_ExploreTopicsSection> createState() => _ExploreTopicsSectionState();
-}
-
-class _ExploreTopicsSectionState extends State<_ExploreTopicsSection> {
-  List<String> topicNames = [
-    'Personal growth',
-    'Culture & Society',
-    'Fiction',
-    'Mind & Philosophy',
-    'Health & Fitness',
-    'Biographies',
-    'Education',
-    'History',
-    'Future',
-    'Technology',
-    'Life style',
-  ];
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 5, top: 40, bottom: 40),
+      padding: const EdgeInsets.only(left: 16, right: 22, top: 40, bottom: 40),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -110,7 +114,7 @@ class _ExploreTopicsSectionState extends State<_ExploreTopicsSection> {
             style: TextStyle(
               fontFamily: 'NB',
               fontSize: 20,
-              color: Colors.white,
+              color: AppColors.white,
             ),
           ),
           const SizedBox(height: 16),
@@ -148,6 +152,173 @@ class _ExploreTopicsSectionState extends State<_ExploreTopicsSection> {
                 ),
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ExploreTopicLists extends StatelessWidget {
+  const _ExploreTopicLists({
+    required this.topicNames,
+  });
+  final List<String> topicNames;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 24),
+            child: SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          topicNames[index],
+                          style: const TextStyle(
+                            fontFamily: 'NB',
+                            fontSize: 20,
+                            color: AppColors.white,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              'show all',
+                              style: TextStyle(
+                                fontFamily: 'NB',
+                                fontSize: 12,
+                                color: AppColors.greenAccent,
+                              ),
+                            ),
+                            const SizedBox(width: 3),
+                            SvgPicture.asset(
+                              'assets/images/icon_arrow_circle_right.svg',
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    height: 254,
+                    width: double.infinity,
+                    child: CustomScrollView(
+                      scrollDirection: Axis.horizontal,
+                      slivers: [
+                        SliverList(
+                          delegate: SliverChildBuilderDelegate(
+                            (context, index) {
+                              return Padding(
+                                padding:
+                                    EdgeInsets.only(left: index == 0 ? 15 : 8),
+                                child: const ProductExploreBox(),
+                              );
+                            },
+                            childCount: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        childCount: topicNames.length,
+      ),
+    );
+  }
+}
+
+class ProductExploreBox extends StatelessWidget {
+  const ProductExploreBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 240,
+      width: 140,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 164,
+            width: double.infinity,
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: Image.asset(
+                'assets/images/The Good Guy Cover.png',
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            'The good guy',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontFamily: 'NM',
+              fontSize: 12,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 4),
+          const Text(
+            'mark fellani',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontFamily: 'NR',
+              fontSize: 10,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              const SizedBox(width: 3),
+              SvgPicture.asset(
+                'assets/images/icon_headphone.svg',
+                height: 14,
+                width: 14,
+              ),
+              const SizedBox(width: 4),
+              const Text(
+                '5m',
+                style: TextStyle(
+                  fontFamily: 'NR',
+                  fontSize: 10,
+                  color: AppColors.greyLight,
+                ),
+              ),
+              const SizedBox(width: 16),
+              SvgPicture.asset(
+                'assets/images/icon_glasses.svg',
+                height: 14,
+                width: 14,
+              ),
+              const SizedBox(width: 4),
+              const Text(
+                '5m',
+                style: TextStyle(
+                  fontFamily: 'NR',
+                  fontSize: 10,
+                  color: AppColors.greyLight,
+                ),
+              ),
+            ],
           ),
         ],
       ),
