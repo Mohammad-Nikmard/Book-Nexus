4cfe86df081a52323fd5eeeac190fd5a17cea217
MohammadNikmard <mnikmard2004@gmail.com>
2025-01-23 12:08:21 +0330

    feature: ad story card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:book_nexsus/ui/home_screen.dart';
+import 'package:book_nexsus/widgets/story_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: StoryCard(),
     );
   }
 }
